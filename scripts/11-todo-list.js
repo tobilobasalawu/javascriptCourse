672222f996090e81b482ffd76c52dc8c37b4e93e
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2024-07-03 22:26:06 +0100

    Update Arrays and loops

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,13 @@
-const todoList = [];
+const todoList = [ 'make dinner', 'wash dishes'];
+
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;
+  todoListHTML += html
+}
+console.log(todoListHTML);
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input')
@@ -10,3 +19,5 @@ function addTodo() {
 
   inputElement.value = '';
 }
+
+
