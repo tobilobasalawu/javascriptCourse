c1c3b9b8f04e578283658b782526653fdfa3e9d0
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2024-07-12 21:52:26 +0100

    Arrow Function in 12 - todoList

--- scripts/12-todo-list .js
+++ scripts/12-todo-list .js
@@ -10,7 +10,7 @@ const todoList = JSON.parse(localStorage.getItem('todoList')) || [{
 function renderTodoList() {
   let todoListHTML = '';
 
-  todoList.forEach(function(todoObject, index){
+  todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
     const html = `
 
