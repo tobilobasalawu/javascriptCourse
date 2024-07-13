308ec0cd78365297776010d024ae68cd098e09e1
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2024-07-13 20:10:07 +0100

    Adding Keydown in 12-RPS

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -24,6 +24,16 @@ function autoPlay() {
   }
 }
 
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+})
+
 document.querySelector('.js-rock-button')
   .addEventListener('click', () => {
     playGame('rock');
